be74ab5284e40307e3785525bbf42d4a23bfb857
Aakash.S <aakashselvaraj2005@gmail.com>
2025-04-25 15:23:06 +0530

    Create LeftSideMountain.java
    Code to print the Left Side Mountain Pattern in Java

---
+++ LeftSideMountain.java
@@ -0,0 +1,33 @@
+Code to print the Left Side Mountain Pattern in Java
+Input : n = 5
+Output : *
+         *
+         * *
+         * * *
+         * * * * 
+         * * * * * 
+         * * * * 
+         * * * 
+         * * 
+         *
+
+CODE :
+import java.util.*;
+public class LeftSideMountain{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
